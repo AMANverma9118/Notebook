03fe0f7fb0b1c5ea8da40eb3a9948e917d81ce31
AMANverma9118 <AMANverma9118359330@gmail.com>
2024-10-16 15:14:18 +0530

    Navbar and Routes is Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Navbar from './Components/Navbar'; 
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my aman</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
